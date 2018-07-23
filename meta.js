3f6d3ea70e73e266ae2fd9b83891fcc3900ef456
游聪聪 <youcongcong@ascs.tech>
2018-07-23 12:07:52 +0800

    feat: 优化

--- meta.js
+++ meta.js
@@ -75,22 +75,6 @@ module.exports = {
       ],
     },
   },
-  filters: {
-    '.eslintrc.js': 'lint',
-    '.eslintignore': 'lint',
-    'config/test.env.js': 'unit || e2e',
-    'build/webpack.test.conf.js': "unit && runner === 'karma'",
-    'test/unit/**/*': 'unit',
-    'test/unit/index.js': "unit && runner === 'karma'",
-    'test/unit/jest.conf.js': "unit && runner === 'jest'",
-    'test/unit/karma.conf.js': "unit && runner === 'karma'",
-    'test/unit/specs/index.js': "unit && runner === 'karma'",
-    'test/unit/setup.js': "unit && runner === 'jest'",
-    'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
-    'src/store/**/*': 'vuex',
-    'src/service/**/*': 'axios',
-  },
   complete: function(data, { chalk }) {
     const green = chalk.green
 
